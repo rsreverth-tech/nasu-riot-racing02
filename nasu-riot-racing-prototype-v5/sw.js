fb06fed7f9367bea13c6da815ddaa9325949b209
rsreverth-tech <rs.reverth@gmail.com>
2026-09-20 09:21:22 +0900

    Add files via upload

---
+++ nasu-riot-racing-prototype-v5/sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'nasu-riot-racing-v5';
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.webmanifest',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/driver/president-neutral-v3.png',
+  './assets/driver/president-happy-v3.png',
+  './assets/driver/president-angry-v3.png',
+  './assets/car-v2/steer-hard-left.png',
+  './assets/car-v2/steer-left.png',
+  './assets/car-v2/steer-center.png',
+  './assets/car-v2/steer-right.png',
+  './assets/car-v2/steer-hard-right.png',
+  './assets/car-v2/jump-up.png',
+  './assets/car-v2/jump-level.png',
+  './assets/car-v2/jump-down.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
+});
